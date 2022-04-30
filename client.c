51d25e753f3fd9bab2e8d83d1a4c2b4dcbfe5f39
Julio Herrera <jurhs2000@gmail.com>
2022-04-30 14:53:05 -0600

    feat: mock to multiple buttons in UI and showing list of users

--- client.c
+++ client.c
@@ -13,9 +13,28 @@
 #include <arpa/inet.h>
 #include <gtk/gtk.h>
 
-static void button_action(GtkWidget *widget, gpointer data)
-{
-    g_print("Button clicked\n");
+GtkWidget *window;
+GtkWidget *grid;
+
+static void showUsers(void) {
+    int usersSize = 10;
+    char labelText[50];
+    GtkWidget *label;
+    for (int i=0; i < usersSize; ++i) {
+        snprintf(labelText, 50, "Este es el usuario %d", i);
+        label = gtk_label_new(labelText);
+        gtk_grid_attach (GTK_GRID (grid), label, 0, i+1, 2, 1);
+    }
+}
+
+static void cleanUsers(GtkWidget *widget, gpointer data) {
+    int usersSize = 10;
+    for (int i=0; i < usersSize; ++i) {
+        gtk_grid_remove_row(GTK_GRID (grid), 1);
+    }
+}
+
+static void show_message() {
     GtkWidget *dialog;
     GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_MODAL;
     char error[] = "error1";
@@ -32,18 +51,30 @@ static void button_action(GtkWidget *widget, gpointer data)
     gtk_widget_show(dialog);
 }
 
+static void button_action(GtkWidget *widget, gpointer data)
+{
+    g_print("Button clicked\n");
+    showUsers();
+}
+
 static void activate(GtkApplication *app, gpointer user_data)
 {
-    GtkWidget *window;
-    GtkWidget *button;
+    GtkWidget *showUsersButton, *switchStatusButton;
 
     window = gtk_application_window_new (app);
     gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
 
-    button = gtk_button_new_with_label ("Hello World");
-    g_signal_connect (button, "clicked", G_CALLBACK (button_action), NULL);
-    gtk_window_set_child (GTK_WINDOW (window), button);
+    grid = gtk_grid_new();
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+
+    showUsersButton = gtk_button_new_with_label ("Show Users");
+    g_signal_connect (showUsersButton, "clicked", G_CALLBACK (button_action), NULL);
+    gtk_grid_attach (GTK_GRID (grid), showUsersButton, 0, 0, 1, 1);
+
+    switchStatusButton = gtk_button_new_with_label ("Switch Status");
+    g_signal_connect (switchStatusButton, "clicked", G_CALLBACK (cleanUsers), NULL);
+    gtk_grid_attach (GTK_GRID (grid), switchStatusButton, 1, 0, 1, 1);
 
     gtk_window_present (GTK_WINDOW (window));
 }
