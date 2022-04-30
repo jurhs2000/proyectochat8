37a1a315a936e355b51b3c983c60327b4dfaa453
Julio Herrera <jurhs2000@gmail.com>
2022-04-29 19:48:00 -0600

    feat: implemented threads on server

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ And to compile the code use the following command
 ```sh
 gcc -o client client.c `pkg-config --cflags --libs gtk4`
 ```
+To compile the server we need to link the pthread library using the next command
+```sh
+gcc -o server server.c -lpthread
+```
--- server.c
+++ server.c
@@ -13,11 +13,15 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *connection_handler(void *);
 
 int main(int argc, char *argv[])
 {
-    int socket_desc, c, new_socket;
-    char *message, client_reply[2000];
+    int socket_desc, c, new_socket, *thread_socket;
+    char *message, *client_ip, client_reply[2000];
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_desc < 0)
     {
@@ -52,19 +56,75 @@ int main(int argc, char *argv[])
         {
             puts("recv failed");
         }
-        puts("Reply received from client\n");
+        client_ip = inet_ntoa(client.sin_addr);
+        puts("Client ip is: ");
+        puts(client_ip);
+        puts("Message received from client\n");
         puts(client_reply);
 		
 		//Reply to the client
 		message = "Hello Client , I have received your connection. But I have to go now, bye\n";
 		write(new_socket , message , strlen(message));
+
+        pthread_t sniffer_thread;
+		thread_socket = malloc(1);
+		*thread_socket = new_socket;
+		
+		if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) thread_socket) < 0)
+		{
+			perror("could not create thread");
+			return 1;
+		}
+		
+		//Now join the thread , so that we dont terminate before the thread
+		//pthread_join( sniffer_thread , NULL);
+		puts("Handler assigned");
 	}
-	
+	close(socket_desc);
 	if (new_socket<0)
 	{
 		perror("accept failed");
 		return 1;
 	}
-
     return 0;
+}
+
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+	//Get the socket descriptor
+	int sock = *(int*)socket_desc;
+	int read_size;
+	char *message , client_message[2000];
+	
+	//Send some messages to the client
+	message = "Greetings! I am your connection handler\n";
+	write(sock , message , strlen(message));
+	
+	message = "Now type something and i shall repeat what you type \n";
+	write(sock , message , strlen(message));
+	
+	//Receive a message from client
+	while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
+	{
+		//Send the message back to client
+		write(sock , client_message , strlen(client_message));
+	}
+	
+	if(read_size == 0)
+	{
+		puts("Client disconnected");
+		fflush(stdout);
+	}
+	else if(read_size == -1)
+	{
+		perror("recv failed");
+	}
+		
+	//Free the socket pointer
+	free(socket_desc);
+	
+	return 0;
 }
